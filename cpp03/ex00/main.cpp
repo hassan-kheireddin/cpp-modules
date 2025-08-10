d762e619a1ea096127c0ef2b2b03ae966ae76eeb
Hassan Kheireddin <hassankheireddin0@gmail.com>
2025-08-10 11:19:46 +0300

    test for ex00

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -11,29 +11,31 @@
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+#include <iostream>
 
 int main() {
-    // Test constructors
-    ClapTrap defaultTrap;
-    ClapTrap namedTrap("Robo");
-    ClapTrap copyTrap(namedTrap);
-
-    // Test assignment
-    defaultTrap = namedTrap;
-
-    // Test functionality
-    namedTrap.attack("Enemy");
-    namedTrap.takeDamage(5);
-    namedTrap.beRepaired(3);
-    namedTrap.takeDamage(10);  // Should destroy
-    namedTrap.attack("Enemy"); // Should fail
-    namedTrap.beRepaired(5);   // Should fail
-
-    // Test energy points
-    ClapTrap energyTester("Battery");
+    std::cout << "=== Testing ClapTrap ===" << std::endl;
+    
+    ClapTrap clap1("CT-1000");
+    ClapTrap clap2("CT-2000");
+    
+    clap1.attack("CT-2000");
+    clap2.takeDamage(5);
+    
+    clap2.attack("CT-1000");
+    clap1.takeDamage(3);
+    
+    clap1.beRepaired(2);
+    clap2.beRepaired(4);
+    
+    // Testing edge cases
+    ClapTrap clap3("CT-3000");
     for (int i = 0; i < 12; i++) {
-        energyTester.attack("Target");
+        clap3.attack("Target");
     }
-
+    
+    clap3.takeDamage(15);
+    clap3.beRepaired(5);
+    
     return 0;
 }
